e8638f76ecd971d04520d4889b2431d25d7d3c3b
Elyse Salberg <elyse_salberg@putnam.com>
2015-03-13 11:16:00 -0400

    Add lvm fact hack

--- manifests/volume_group.pp
+++ manifests/volume_group.pp
@@ -4,25 +4,32 @@ define lvm::volume_group (
   $physical_volumes,
   $ensure           = present,
   $logical_volumes  = {},
+  $unless_vg       = undef,
+  $createonly      = false,
+  $lv_fact_match   = false,
 ) {
 
   validate_hash($logical_volumes)
 
-  physical_volume { $physical_volumes:
-    ensure => $ensure,
-  }
-
-  volume_group { $name:
-    ensure           => $ensure,
-    physical_volumes => $physical_volumes,
-  }
-
-  create_resources(
-    'lvm::logical_volume',
-    $logical_volumes,
-    {
-      ensure       => $ensure,
-      volume_group => $name,
+  if ! str2bool($lv_fact_match) {
+    physical_volume { $physical_volumes:
+      ensure    => $ensure,
+      unless_vg => $unless_vg,
     }
-  )
+
+    volume_group { $name:
+      ensure           => $ensure,
+      physical_volumes => $physical_volumes,
+      createonly       => $createonly,
+    }
+
+    create_resources(
+      'lvm::logical_volume',
+      $logical_volumes,
+      {
+        ensure       => $ensure,
+        volume_group => $name,
+      }
+    )
+  }
 }
