0f2aad464c47b8d7cc5f77d6ce6cba5168736ab6
Elyse Salberg <elyse_salberg@putnam.com>
2015-03-12 23:59:59 -0400

    Work on fact testing

--- manifests/volume_group.pp
+++ manifests/volume_group.pp
@@ -11,7 +11,7 @@ define lvm::volume_group (
 
   validate_hash($logical_volumes)
 
-  if ! str2bool($lv_fact_match) {
+  if $lv_fact_match == 'yes' {
     physical_volume { $physical_volumes:
       ensure => $ensure,
     }
