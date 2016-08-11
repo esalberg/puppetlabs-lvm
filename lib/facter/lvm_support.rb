297512937801a99bb7d28533c92affae3af617cc
Elyse Salberg <elyse_salberg@putnam.com>
2016-08-11 14:57:58 -0400

    testing

--- lib/facter/lvm_support.rb
+++ lib/facter/lvm_support.rb
@@ -21,6 +21,8 @@ Facter.add('lvm_vg_list') do
       0
     else
       vg_list = vglist.strip
+      vg_array = vg_list.split
+      vg_array
 #      vg_list.delete_if{|e| e.length == 0}
 #      vg_list
     end
