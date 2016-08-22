a2761ff5f9df6b67b02e651c746706d92a3a81b4
Elyse Salberg <elyse_salberg@putnam.com>
2016-08-22 13:05:02 -0400

    test fact

--- lib/facter/lvm_support.rb
+++ lib/facter/lvm_support.rb
@@ -13,10 +13,11 @@ end
 #   Number of VGs
 vg_list = []
 Facter.add('lvm_vgs') do
-  confine :kernel => "Linux"
+#  confine :kernel => "Linux"
   confine :lvm_support => true
 #  setcode do
-  vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', options = {:timeout => 30})
+#  vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', options = {:timeout => 30})
+  vgs = Facter::Util::Resolution.exec('vgs -o name --noheadings 2>/dev/null')
   if vgs.nil?
     setcode { 0 }
   else
