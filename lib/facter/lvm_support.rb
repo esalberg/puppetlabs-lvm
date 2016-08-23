fb867219fd8e3f28e87ffd7488e4ed8ba8ee2f5f
Elyse Salberg <elyse_salberg@putnam.com>
2016-08-22 21:43:13 -0400

    fix typos

--- lib/facter/lvm_support.rb
+++ lib/facter/lvm_support.rb
@@ -17,7 +17,7 @@ Facter.add('lvm_vgs') do
 
   lvm = Facter.value(:lvm_support)
   if lvm 
-    vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', timeout: 30})
+    vgs = Facter::Core::Execution.execute('vgs -o name --noheadings 2>/dev/null', timeout: 30)
   end
   if vgs.nil?
     setcode { 0 }
@@ -33,7 +33,7 @@ vg_list.each_with_index do |vg, i|
   Facter.add("lvm_vg_#{i}") { setcode { vg } }
   Facter.add("lvm_vg_#{vg}_pvs") do
     setcode do
-      pvs = Facter::Util::Resolution.exec("vgs -o pv_name #{vg} 2>/dev/null", timeout: 30})
+      pvs = Facter::Util::Resolution.exec("vgs -o pv_name #{vg} 2>/dev/null", timeout: 30)
       res = nil
       unless pvs.nil?
         res = pvs.split("\n").select{|l| l =~ /^\s+\// }.collect(&:strip).sort.join(',')
@@ -51,7 +51,7 @@ Facter.add('lvm_pvs') do
 
   lvm = Facter.value(:lvm_support)
   if lvm 
-    pvs = Facter::Core::Execution.execute('pvs -o name --noheadings 2>/dev/null', timeout: 30})
+    pvs = Facter::Core::Execution.execute('pvs -o name --noheadings 2>/dev/null', timeout: 30)
   end
   if pvs.nil?
     setcode { 0 }
